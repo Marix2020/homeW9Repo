65baf272afec573704898466eabf57973f12bb04
Marix2020 <maricelocampo888@gmail.com>
2020-07-03 18:02:35 -0400

    HW Practice9

---
+++ src/main/java/GITHomeW9Project/GITHomeW9Project/App.java
@@ -0,0 +1,13 @@
+package GITHomeW9Project.GITHomeW9Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
